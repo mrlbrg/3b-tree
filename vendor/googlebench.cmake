0b40a861caac5e4120875fdbc638655f2f444912
Marlene Bargou <marlene.bargou@tum.de>
2025-10-01 17:36:39 +0200

    Build in release mode

--- vendor/googlebench.cmake
+++ vendor/googlebench.cmake
@@ -11,9 +11,10 @@ FetchContent_Declare(
 )
 
 set(BENCHMARK_ENABLE_TESTING OFF)
-set(SAVED_FLAGS "${CMAKE_CXX_FLAGS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-error")
+
 
 FetchContent_MakeAvailable(benchmark)
 
-set(CMAKE_CXX_FLAGS "${SAVED_FLAGS}")
+set_target_properties(benchmark PROPERTIES
+    COMPILE_OPTIONS "-Wno-unused-but-set-variable"
+)
